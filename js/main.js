3ec1c1ec447372bf59f0e622144ea8232907acba
vitaretnama <nurvitaretnama@gmail.com>
2023-10-11 11:38:15 +0700

    fix faq

--- js/main.js
+++ js/main.js
@@ -16,3 +16,19 @@ closeBtn.addEventListener("click", () => {
     closeBtnIcn.classList.toggle(arrowRightClass);
   }
 });
+
+
+// FAQ
+
+const faqItems = document.querySelectorAll('.faq__item');
+
+faqItems.forEach((item) => {
+  const faqBtn = item.querySelectorAll;
+
+  item.addEventListener('click', () => {
+    const isOpen = item.classList.toggle('open');
+    const iconClass = isOpen ? 'ri-substract-fill' : 'ri-arrow-right-s-line';
+    const iconElement = faqBtn.querySelector('i');
+    iconElement.classList = `${iconClass} text-2xl`
+  })
+})
